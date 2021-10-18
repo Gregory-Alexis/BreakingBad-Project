3d42e2c2cee2890afe60fe4f10b32cf08e54c5b2
Westindiess <gregoryalexiss@gmail.com>
2021-10-18 22:20:10 +0200

    breaking bad

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import Logo from "../../images/logo.png"
 const Header = () => {
 	return (
 		<header className="flex justify-center pt-5">
-			<img src={Logo} alt="logo breaking bad" />
+			<img src={Logo} alt="logo breaking bad" className="w-96 h-full" />
 		</header>
 	)
 }
